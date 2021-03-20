47eeac36594405fa2d719af0b14b68def412aba5
Crowdbotics <team@crowdbotics.com>
2021-03-20 19:01:01 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/fshnmagazine_25149/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fshnmagazine_25149;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "fshnmagazine_25149";
+    }
+}
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://fshnmagazine-25149.botics.co",
+  defaultTimeout: 5000
+};
